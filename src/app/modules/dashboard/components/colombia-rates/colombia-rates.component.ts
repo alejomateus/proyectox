05c63cbe8a7ef0696d7185301849dabfa1f30fb2
Miguel Corzo <johndoe@avaldigitallabs.com>
2020-09-09 16:16:48 -0500

    seccion precios colombia

--- src/app/modules/dashboard/components/colombia-rates/colombia-rates.component.ts
+++ src/app/modules/dashboard/components/colombia-rates/colombia-rates.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-colombia-rates',
   templateUrl: './colombia-rates.component.html',
-  styleUrls: ['./colombia-rates.component.scss']
+  styleUrls: [
+    './colombia-rates.component.scss',
+    '../abroad-rates/abroad-rates.component.scss',
+  ],
 })
 export class ColombiaRatesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
